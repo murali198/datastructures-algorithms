e974f705b62c05a5446d550e3b9c6f564a4d8c2c
murali <murali@murali>
2019-08-22 11:06:09 +0530

    TEST

---
+++ datastructures-algorithms-master/src/com/murali/ds/array/NumberArrayAlogorithms.java
@@ -0,0 +1,52 @@
+package com.murali.ds.array;
+
+import java.util.Arrays;
+
+public class NumberArrayAlogorithms {
+
+	public static void main(String[] args) {
+		dublicateElement();
+	}
+
+	public static void reverseArray() {
+
+		Integer[] arr = new Integer[] {1,2,3,4,5};
+		int temp, size = arr.length;
+		for(int i = 0; i < size/2; i++) {
+			temp = arr[i];
+			arr[i] = arr[size - i - 1];
+			arr[size - i - 1] = temp;
+		}
+
+		for(int i = 0; i < size; i++) {
+			System.out.print(arr[i] + " ");
+		}
+	}
+
+	public static void removeDublicateWithSort() {
+		Integer[] arr = new Integer[] {1,1,2,3,4,5, 5, 5, 3};
+		Arrays.sort(arr);
+		int count = 0;
+		for(int i = 0; i < arr.length; i++) {
+			if(i == 0 || arr[i] != arr[i - 1]) {
+				arr[count++] = arr[i];
+			}
+		}
+		arr = Arrays.copyOf(arr, count);
+
+		for(int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+	}
+
+	public static void dublicateElement( ) {
+		int arr[] = {1, 2, 3, 1, 3, 6, 99};
+		for (int i = 0; i < arr.length; i++) { 
+            if (arr[Math.abs(arr[i])] >= 0) 
+                arr[Math.abs(arr[i])] = -arr[Math.abs(arr[i])]; 
+            else
+                System.out.print(Math.abs(arr[i]) + " "); 
+        }
+	}
+
+}
